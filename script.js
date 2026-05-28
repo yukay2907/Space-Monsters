49538b3454116643c295f4253c87be9f854c4141
Utkarsh Kashyap <116485473+yukay2907@users.noreply.github.com>
2026-05-28 14:02:34 +0530

    attempt at understanding fetching using a mini project

---
+++ script.js
@@ -0,0 +1,80 @@
+const container = document.querySelector(".container");
+const input = document.querySelector("input");
+
+let usersArray = [];
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+const createCardList = (array) => {
+  container.innerHTML = "";
+
+  array.forEach((obj) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `<div class="name">Name</div><div class="name-content">${obj.username}</div><div class="email">Email</div><div class="email-content">${obj.email}</div>`;
+    container.appendChild(card);
+  });
+};
+
+fetch(url)
+  .then((data) => {
+    return data.json();
+  })
+  .then((result) => {
+    console.log(result);
+    usersArray = result;
+    createCardList(usersArray);
+  });
+
+input.addEventListener("input", (event) => {
+  const searchStr = event.target.value.toLowerCase();
+
+  const filteredArray = usersArray.filter((ele) => {
+    return (
+      ele.username.toLowerCase().includes(searchStr) ||
+      ele.email.toLowerCase().includes(searchStr)
+    );
+  });
+
+  createCardList(filteredArray);
+});
+
+// particle js configuration
+particlesJS.load("particles-js", "particles.json");
+
+const addUserButton = document.querySelector(".controls img");
+
+addUserButton.addEventListener("click", () => {
+  const username = prompt("enter your username");
+  const email = prompt("enter your email");
+
+  const newUser = {
+    username,
+    email,
+  };
+
+  const secretKey = prompt("enter secret key");
+
+  const bodyData = {
+    newUser,
+    secretKey,
+  };
+
+  fetch(`${url}/adddata`, {
+    method: "POST",
+    body: JSON.stringify(bodyData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((data) => data.json())
+    .then((result) => {
+      usersArray = result;
+      createCardList(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("User not added");
+    });
+});
